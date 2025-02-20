e2aedbd4aa3607be21cfaef3c1e227148fd1b431
romel <romelhasan741@gmail.com>
2025-02-20 17:02:15 +0600

    app vue solved

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import router from './router'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app');
