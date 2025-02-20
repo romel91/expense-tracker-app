ee4a9abb1949a6ccaaede2b2b85759c1770276cb
romel <romelhasan741@gmail.com>
2025-02-20 17:28:57 +0600

    app vue solved2

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app');
+createApp(App).mount('#app'); // Ensure it targets #app in index.html
